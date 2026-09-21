11df529fbd39e54584f5ac0ee1e618cb7abb34e0
Filippo Cinotti <cinottolo96@gmail.com>
2026-09-21 06:13:32 +0200

    fix: remove redundant Crashlytics imports

--- apps/mobile/lib/core/crash_reporting.dart
+++ apps/mobile/lib/core/crash_reporting.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
-import 'dart:ui';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
 
 /// Production-only crash collection with no user, recipe, diet or health keys.
 class CrashReporting {
