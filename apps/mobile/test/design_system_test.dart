19aaba11423dc3c29f058217109e07cb34fe14e2
Filippo Cinotti <cinottolo96@gmail.com>
2026-09-22 17:16:24 +0200

    test: remove redundant design system import

--- apps/mobile/test/design_system_test.dart
+++ apps/mobile/test/design_system_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:eatme/core/localization.dart';
-import 'package:eatme/design_system/premium.dart';
 import 'package:eatme/design_system/theme.dart';
 import 'package:eatme/design_system/widgets.dart';
 
